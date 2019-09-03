777fdb35b26e61d2954ff751b36980faaaebbb1b
marita30 <marita.jose.ch@gmail.com>
2019-09-03 14:40:57 -0600

    Create default user in the file seeds.rb and create new model and controller for the role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+role = Role.create([{ name_roles: 'admin'}, { name_roles: 'user'}])
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
