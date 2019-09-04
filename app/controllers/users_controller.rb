99a5316d8981ac17590852c1395e1679d87dd7ad
marita30 <marita.jose.ch@gmail.com>
2019-09-04 14:19:24 -0600

    change in the method user_params the password_diguest for only password and add password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :password_digest, :role_id)
+      params.require(:user).permit(:email, :password, :password_confirmation :role_id)
     end
 end
