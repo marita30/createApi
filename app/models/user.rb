7615cb0fb2ba78a2182932d6561fab59db3cb9e7
marita30 <marita.jose.ch@gmail.com>
2019-09-04 14:07:23 -0600

    Add a new method for add one email for generate token in the authentication in te file user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  # metodo de autentificacion
+  has_secure_password
   belongs_to :role
+  validates :email, presence: true
+
+  def to_token_payload
+    {
+      sub: id,
+      email: email
+    }
+  end
 end
