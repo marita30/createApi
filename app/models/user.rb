9f2a50bf0899afcfa1610244adcf09d8d489a62e
marita30 <marita.jose.ch@gmail.com>
2019-09-04 15:32:07 -0600

    Add in the file user.rb the has_secured_password and a new method call to_token_payload for create the token payload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true
   belongs_to :role
+
+  def to_token_payload
+    {
+      sub: id,
+      email: email
+    }
+  end
 end
