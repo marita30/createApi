0f3b992a06ecea0d584c79496dc12303a90c2fc7
marita30 <marita.jose.ch@gmail.com>
2019-09-05 12:14:59 -0600

    Create a new method for verify if a user or administrator with current_user.role.name_roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
     include Knock::Authenticable
+
+    #Methodo para ver si el usuario es administrador o un usuario normal.
+   
+    def is_admin?
+
+        if current_user.role.name_roles == "user"
+            render json: {"error": "you are not admin",
+                           "values": []}, status: :unauthorized
+        end
+   end
 end
