e664d561f5bc5b971836534d26ae8315a251ac80
marita30 <marita.jose.ch@gmail.com>
2019-09-05 12:13:31 -0600

    Add the before_action :is_admin and editing the method Index, Patch editing the structure json for verify if is administrator or normal user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,28 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   before_action :authenticate_user
 
+  #Verificar en el metodo si es un administrador o un usuario :is_admin en el archivo Application_controller en el method update y index.
+  before_action :is_admin?, only: [:update, :index]
+
+
   # GET /users
   def index
-    byebug
+  
     @users = User.all
-
-    render json: @users
+    #Estructura de json.
+    render json:  {"error": [], "values": @users}, status: :ok
   end
 
   # GET /users/1
   def show
    
     render json: @user
+
   end
 
   # POST /users
   def create
+
     @user = User.new(user_params)
 
     if @user.save
@@ -25,30 +31,41 @@ class UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+
   end
 
   # PATCH/PUT /users/1
   def update
+
     if @user.update(user_params)
-      render json: @user
+      #Error no contiene nada porque es un admin y los valores que trae la variable @user.
+      render json:  {"error": [], "values": @user}, status: :accepted
     else
-      render json: @user.errors, status: :unprocessable_entity
+       #Error si contiene porque no es un admin y los valores null.
+      render json: {"error": @user.errors, "values":[]}, status: :unprocessable_entity
     end
+
   end
 
   # DELETE /users/1
   def destroy
+
     @user.destroy
+
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
+
       @user = User.find(params[:id])
+
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
+
       params.require(:user).permit(:email, :password, :password_confirmation, :role_id)
+      
     end
 end
