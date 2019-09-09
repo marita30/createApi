61fb7542141ea32c80d0b735f2636515277d2ea5
marita30 <marita.jose.ch@gmail.com>
2019-09-09 08:30:51 -0600

    Add the validates for the datetimaEntrance for havent values nil

--- app/models/entrada_salida.rb
+++ app/models/entrada_salida.rb
@@ -2,6 +2,7 @@ class EntradaSalida < ApplicationRecord
   belongs_to :user
   #Validar que los campos datetime y user_id no vengan nil
   validates :user_id, presence: true
+  validates :datetimeEntrance, presence: true
 
 
 end
