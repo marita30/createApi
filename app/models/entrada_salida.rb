cc23adb70bb2e82179b6cb10c5e24d4dc0c0f72d
marita30 <marita.jose.ch@gmail.com>
2019-11-06 21:30:45 -0600

    Delete the date, presence: true para que agarre valores vacios en la fecha

--- app/models/entrada_salida.rb
+++ app/models/entrada_salida.rb
@@ -2,7 +2,7 @@ class EntradaSalida < ApplicationRecord
   belongs_to :user
   #Validar que los campos datetime y user_id no vengan nil
   validates :user_id, presence: true
-  validates :date, presence: true
+  # validates :date, presence: true
 
 
 end
